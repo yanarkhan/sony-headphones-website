4578f58b366063d2db604bbe14af20c8d1af00a1
yanarkhan <rianarkhan@gmail.com>
2024-04-03 19:37:02 +0700

    Menambahkan script.js untuk menampilkan dan menyembunyikan menu saat hamburger menu diklik atau tombol close ditekan. Menambahkan juga gaya CSS untuk mengatur tampilan menu yang ditampilkan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,42 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* Menu Show */
+// Validasi jika ada hamburger menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+// Validasi jika ada tombol close
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav-link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // ketika klik setiap nav-link, maka menghapus kelas show-menu
+  navMenu.classList.remove("show-menu");
+};
+
+// menambahkan event listener untuk setiap elemen dalam navLink
+navLink.forEach((elemen) => elemen.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
 
+/*=============== SWIPER FAVORITES ===============*/
 
-/*=============== SWIPER FAVORITES ===============*/ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
